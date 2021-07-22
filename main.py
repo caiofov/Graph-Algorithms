5b393989858f52f2b24a693bf695bb5d7e14c04d
Caio Oliveira <69114136+caiofov@users.noreply.github.com>
2021-07-22 01:59:11 +0000

    correcting issues and updating readme

--- main.py
+++ main.py
@@ -3,24 +3,9 @@ from graphAlgorithms import *
 
 graph = samples.g231
 
-# for edge in graph.edgesList:
-#   v,u = edge.endpoints
-#   print(v.name, " - ", u.name, "|", edge.weight)
-
 
 mst = graph.kruskalMinimumSpanningTree()
-<<<<<<< HEAD
 
-print()
+
 mst.displayEdges()
-mst.display()
-
-
-print(len(mst.edgesList))
-=======
->>>>>>> origin/master
-
-
-for edge in mst.edgesList:
-  v,u = edge.endpoints
-  print(v.name, " - ", u.name, "|", edge.weight)
+# mst.display()
